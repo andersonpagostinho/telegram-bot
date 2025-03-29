5abfef54a1a09b5a2b7ed0cb098978c794149c1e
Anderson agostinho <andersonpagostinho@gemail.com>
2025-03-29 12:16:32 -0300

    Corrigido Firebase e adicionadas funções de tarefas

--- converter.py
+++
@@ -1,8 +0,0 @@
-from pydub import AudioSegment
-
-# Converter M4A para WAV
-audio = AudioSegment.from_file("arquivo.m4a", format="m4a")
-audio = audio.set_channels(1).set_frame_rate(16000)  # Mono e 16kHz
-audio.export("teste.wav", format="wav")
-
-print("✅ Conversão concluída: teste.wav")
